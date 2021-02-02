10fc7fb5db128aaf535114b69480f121e8e099f2
Sam Acquaviva <samacqua@gmail.com>
2021-02-01 22:44:39 -0800

    Updated analysis file.

--- analysis.py
+++ analysis.py
@@ -3,6 +3,7 @@ import pandas as pd
 import nltk
 import matplotlib.pyplot as plt
 import pickle
+import numpy as np
 
 
 def load_tasks(task_nums):
@@ -112,64 +113,92 @@ def vocab_analysis(descs_df):
     tot_freq_dist.plot(30, cumulative=False)
 
 
+def compare_grid_sizes(descs_df):
+    by_task = descs_df.groupby('task').successful_communication.any()
+
+    successful_tasks = set([task for task, successful in by_task.to_dict().items() if successful])
+    suc_sizes = []
+    for task in successful_tasks:
+        test_grid = tasks[task]['test'][0]['input']
+        suc_sizes.append(len(test_grid) * len(test_grid[0]))
+
+    unsuccessful_tasks = set(task_nums) - successful_tasks
+    unsuc_sizes = []
+    for task in unsuccessful_tasks:
+        test_grid = tasks[task]['test'][0]['input']
+        unsuc_sizes.append(len(test_grid) * len(test_grid[0]))
+
+    print("Successful tasks test input size mean:", np.mean(suc_sizes))
+    print("Unsuccessful tasks test input size mean:", np.mean(unsuc_sizes))
+
+    plt.title('Test input grid sizes')
+    bins = np.linspace(min(min(unsuc_sizes), min(suc_sizes)), max(max(unsuc_sizes), max(suc_sizes)), num=30)
+    plt.hist(suc_sizes, bins=bins, alpha=0.5, label="Successful")
+    plt.hist(unsuc_sizes, bins=bins, alpha=0.5, label="Unsuccessful")
+    plt.legend(loc='upper right')
+    plt.show()
+
+
 if __name__ == '__main__':
     task_nums = range(400)
 
     # sus way to save filename to account for which tasks are being saved
     # saving name of each task is too long to save
     local_file = f'dataframes_{sum(task_nums)}.data'
+    tasks = load_tasks(task_nums)
+
     try:
         fd = open(local_file, 'rb')
         descs_df, builds_df = pickle.load(fd)
         fd.close()
     except FileNotFoundError:
         print("Not computed yet, creating and saving locally...")
-        tasks = load_tasks(task_nums)
         descs_df, builds_df = create_pandas_df(tasks)
 
-    # by_task = descs_df.groupby('task').successful_communication.any()
-    #
-    # successful_descs, unsuccessful_descs = descs_df[descs_df.successful_communication == True], \
-    #                                        descs_df[descs_df.successful_communication == False]
-    #
-    # see_words_suc, grid_words_suc, do_words_suc = tokenize_descriptions(successful_descs)
-    # see_words_unsuc, grid_words_unsuc, do_words_unsuc = tokenize_descriptions(unsuccessful_descs)
-    # see_words, grid_words, do_words = see_words_suc+see_words_unsuc, grid_words_suc+grid_words_unsuc, \
-    #                                   do_words_suc+do_words_unsuc
-    #
-    # suc_words = see_words_suc + grid_words_suc + do_words_suc
-    # common_suc_words = set([word for word, freq in get_freq_dist(suc_words).most_common(50)])
-    # unsuc_words = see_words_unsuc + grid_words_unsuc + do_words_unsuc
-    # common_unsuc_words = set([word for word, freq in get_freq_dist(unsuc_words).most_common(50)])
-    # words = see_words + grid_words + do_words
-    # common_words = set([word for word, freq in get_freq_dist(words).most_common(50)])
-    #
-    # print("Words in successful descriptions but not in unsuccessful:", common_suc_words-common_unsuc_words)
-    # print("Words in unsuccessful descriptions but not in unsuccessful:", common_unsuc_words-common_suc_words)
-    #
-    # perc_difs = {}
-    # for word in set(do_words_suc):
-    #     word = word[0]
-    #     print(word)
-    #
-    #     # '(' and ')' have special meanings in pandas regex, so have to put \ in front
-    #     if word in ['(', ')']:
-    #         word = '\\' + word
-    #
-    #     suc_descs_w_word = successful_descs[successful_descs['do_description'].str.contains(word)]
-    #     perc_suc_descs = len(suc_descs_w_word.index) / len(successful_descs)
-    #
-    #     unsuc_descs_w_word = unsuccessful_descs[unsuccessful_descs['do_description'].str.contains(word)]
-    #     perc_unsuc_descs = len(unsuc_descs_w_word.index) / len(unsuccessful_descs)
-    #
-    #     perc_dif = perc_suc_descs - perc_unsuc_descs
-    #     if word in ['\(', '\)']:
-    #         word = word[-1]
-    #     perc_difs[word] = perc_dif
-    #
-    # perc_difs = [(k, v) for k, v in perc_difs.items()]
-    # perc_difs = sorted(perc_difs, key=lambda x: x[1])
-    # print("Worst words:", perc_difs[:5])
-    # print("Best words:", perc_difs[-5:])
+    compare_grid_sizes(descs_df)
+
+    # compare n-grams in successful tasks vs unsuccessful tasks
+    successful_descs, unsuccessful_descs = descs_df[descs_df.successful_communication == True], \
+                                           descs_df[descs_df.successful_communication == False]
+
+    see_words_suc, grid_words_suc, do_words_suc = tokenize_descriptions(successful_descs)
+    see_words_unsuc, grid_words_unsuc, do_words_unsuc = tokenize_descriptions(unsuccessful_descs)
+    see_words, grid_words, do_words = see_words_suc+see_words_unsuc, grid_words_suc+grid_words_unsuc, \
+                                      do_words_suc+do_words_unsuc
+
+    suc_words = see_words_suc + grid_words_suc + do_words_suc
+    common_suc_words = set([word for word, freq in get_freq_dist(suc_words).most_common(50)])
+    unsuc_words = see_words_unsuc + grid_words_unsuc + do_words_unsuc
+    common_unsuc_words = set([word for word, freq in get_freq_dist(unsuc_words).most_common(50)])
+    words = see_words + grid_words + do_words
+    common_words = set([word for word, freq in get_freq_dist(words).most_common(50)])
+
+    print("Words in successful descriptions but not in unsuccessful:", common_suc_words-common_unsuc_words)
+    print("Words in unsuccessful descriptions but not in unsuccessful:", common_unsuc_words-common_suc_words)
+
+    perc_difs = {}
+    for word in set(do_words_suc):
+        word = word[0]
+        print(word)
+
+        # '(' and ')' have special meanings in pandas regex, so have to put \ in front
+        if word in ['(', ')']:
+            word = '\\' + word
+
+        suc_descs_w_word = successful_descs[successful_descs['do_description'].str.contains(word)]
+        perc_suc_descs = len(suc_descs_w_word.index) / len(successful_descs)
+
+        unsuc_descs_w_word = unsuccessful_descs[unsuccessful_descs['do_description'].str.contains(word)]
+        perc_unsuc_descs = len(unsuc_descs_w_word.index) / len(unsuccessful_descs)
+
+        perc_dif = perc_suc_descs - perc_unsuc_descs
+        if word in ['\(', '\)']:
+            word = word[-1]
+        perc_difs[word] = perc_dif
+
+    perc_difs = [(k, v) for k, v in perc_difs.items()]
+    perc_difs = sorted(perc_difs, key=lambda x: x[1])
+    print("Worst words:", perc_difs[:5])
+    print("Best words:", perc_difs[-5:])
 
     vocab_analysis(descs_df)
