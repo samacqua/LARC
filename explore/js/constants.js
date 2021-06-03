03d325f69b7c2c8f7b7ebd81cae8600768106b58
Sam Acquaviva <samacqua@gmail.com>
2021-06-02 20:16:30 -0400

    Added explore page for dataset.

---
+++ explore/js/constants.js
@@ -0,0 +1,6 @@
+
+const NUM_TASKS = 10;
+const TASKS = [...Array(NUM_TASKS).keys()];
+const GRID_SIZE_PREFIX = "The output grid size...";
+const SHOULD_SEE_PREFIX = "In the input, you should see...";
+const HAVE_TO_PREFIX = "To make the output, you have to...";
